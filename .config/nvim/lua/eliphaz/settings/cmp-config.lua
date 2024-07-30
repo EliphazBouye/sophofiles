cd44de80461149fde181dd395c03575fa4d13ee7
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-07-30 19:52:02 +0000

    fix: tab command not working propertly in neovim

--- .config/nvim/lua/eliphaz/settings/cmp-config.lua
+++ .config/nvim/lua/eliphaz/settings/cmp-config.lua
@@ -22,7 +22,7 @@ function M.setup ()
           select = true,
         },
         ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible then
+          if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
@@ -31,7 +31,7 @@ function M.setup ()
           end
         end, { 'i', 's' }),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible then
+          if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.expand_or_jumpable(-1) then
             luasnip.jump(-1)
